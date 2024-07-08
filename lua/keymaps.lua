50b18a4a31d496885bad73901bf4896591d18702
Alexandroskw <filipbilly.120@gmail.com>
2024-07-07 22:15:36 -0600

    Little additions to the keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,5 +10,13 @@ vim.opt.expandtab = true
 vim.opt.smarttab = true
 vim.opt.smartindent = true
 
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Ttransparent screen
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
