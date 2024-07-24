e15fc21997d76a691e2e65c1886b84aec21ba899
Alexandroskw <filipbilly.120@gmail.com>
2024-07-23 20:33:18 -0600

    Removing autoinstalling ```rust_analyzer``` for enabling and use the plugin ```rustaceanvim``` (No instalation needed)

--- lua/plugin_config/mason.lua
+++ lua/plugin_config/mason.lua
@@ -2,9 +2,12 @@ require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = {
 		"lua_ls",
-		"rust_analyzer",
+		-- "rust_analyzer",
+        "pyright",
 	}
 })
 
 require("lspconfig").lua_ls.setup{}
-require("lspconfig").rust_analyzer.setup{}
+-- require("lspconfig").rust_analyzer.setup{}
+require("lspconfig").pyright.setup{}
+require("lspconfig").ruff.setup{}
