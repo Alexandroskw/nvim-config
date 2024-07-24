170c531770c4e892f602bd4c68685a39fe16c8fe
Alexandroskw <filipbilly.120@gmail.com>
2024-07-23 20:23:59 -0600

    Adding autoformatting rust code when the file is closed or saved

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,8 +20,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Keeps the cursor in the middle with jump page
-vim.keymap.set("n", "<C-u>", "<C-d>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Keeps the cursor in the middle in search mode
 vim.keymap.set("n", "n", "nzzzv")
@@ -30,6 +30,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 -- Remove the highlights of the search mode
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- Autoformat rust code when the file is saved or closed
+vim.g.rustfmt_autosave = 1
 
 -- Transparent screen
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
