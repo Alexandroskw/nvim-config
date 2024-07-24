ee7333f434727ba4868543a1fdd1a83fca02b941
Alexandroskw <filipbilly.120@gmail.com>
2024-07-23 20:31:17 -0600

    Configuring ```everforest``` to statusbar

--- lua/plugin_config/lualine.lua
+++ lua/plugin_config/lualine.lua
@@ -1,4 +1,26 @@
-require('lualine').setup {
-	options = { theme = 'horizon' }
-}
+local lualine = require("lualine")
 
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "everforest",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {},
+		always_divide_middle = true,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+})
