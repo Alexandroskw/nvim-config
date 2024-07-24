02804cadfa2b3c76a53eec09d71fe525451f5edd
Alexandroskw <filipbilly.120@gmail.com>
2024-07-23 20:35:55 -0600

    Only justifiying the code

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,55 +1,63 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-        use 'wbthomason/packer.nvim'
-        use 'nvim-tree/nvim-tree.lua'
-        use 'nvim-lualine/lualine.nvim'
-        use 'nvim-tree/nvim-web-devicons'
-        use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-        use {"williamboman/mason.nvim",
-                "williamboman/mason-lspconfig.nvim",
-                "neovim/nvim-lspconfig",
-        }
-        use 'EdenEast/nightfox.nvim'
-        use 'lewis6991/gitsigns.nvim'
-        use {
-                'nvim-telescope/telescope.nvim', tag = '0.1.8',
-                -- or                            , branch = '0.1.x',
-                requires = { {'nvim-lua/plenary.nvim'} }
-        }
-        use {
-                'VonHeikemen/lsp-zero.nvim',
-                branch = 'v3.x',
-                requires = {
-                        --- Uncomment the two plugins below if you want to manage the language servers from neovim
-                        -- {'williamboman/mason.nvim'},
-                        -- {'williamboman/mason-lspconfig.nvim'},
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use("nvim-tree/nvim-tree.lua")
+	use("nvim-lualine/lualine.nvim")
+	use("nvim-tree/nvim-web-devicons")
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	})
+	use("EdenEast/nightfox.nvim")
+	use("lewis6991/gitsigns.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			-- {'williamboman/mason.nvim'},
+			-- {'williamboman/mason-lspconfig.nvim'},
 
-                        {'hrsh7th/nvim-cmp'},
-                        {'hrsh7th/cmp-nvim-lsp'},
-                        {'L3MON4D3/LuaSnip'},
-                }
-        }
-        use 'christoomey/vim-tmux-navigator'
-        -- My plugins here
-        -- use 'foo1/bar1.nvim'
-        -- use 'foo2/bar2.nvim'
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	})
+	use("christoomey/vim-tmux-navigator")
+	use("mfussenegger/nvim-dap")
+	use("rcarriga/nvim-dap-ui")
+	use("rust-lang/rust.vim")
+	-- use("simrat39/rust-tools.nvim")
+	use("nvimtools/none-ls.nvim")
+	use("mrcjkb/rustaceanvim")
+	use("nvim-neotest/nvim-nio")
+	-- My plugins here
+	-- use 'foo1/bar1.nvim'
+	-- use 'foo2/bar2.nvim'
 
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
