c7ceae5861906b7dba37e6474430c5a43e52db02
Alexandroskw <filipbilly.120@gmail.com>
2024-07-23 20:46:20 -0600

    removing the lspconfig

---
+++ lua/plugin_config/lspconfig.lua
@@ -0,0 +1,14 @@
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
+
+-- lspconfig.rust_analyzer.setup({
+--         filetypes = { "rust" },
+--         root_dir = util.root_pattern("Cargo.toml"),
+--         setting = {
+--                 ["rust_analyzer"] = {
+--                         cargo = {
+--                                 allFeatures = true,
+--                         },
+--                 },
+--         },
+-- })
