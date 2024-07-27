e4f1c6cbbe80f71f4bbf90d34db1530a7fb94824
Alexandroskw <filipbilly.120@gmail.com>
2024-07-26 20:39:32 -0600

    Upgrading the configuration for Rust Deleting the lspconfig from the files. Rustaceanvim use rust-analyzer by default no need to be configured in Mason

--- lua/plugin_config/lsp.lua
+++ lua/plugin_config/lsp.lua
@@ -1,27 +1,23 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 --lsp_zero.extend_lspconfig()
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
-require('mason').setup({})
-require('mason-lspconfig').setup({
-        ensure_installed = {"lua_ls", "rust_analyzer", "pyright"},
-        handlers = {
-                function (lua_ls)
-                        require('lspconfig')[lua_ls].setup({})
-                end,
-                function(rust_analyzer)
-                        require('lspconfig')[rust_analyzer].setup({})
-                end,
-                function(pyright)
-                        require('lspconfig')[pyright].setup({})
-                end,
-
-        },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls", "pyright" },
+	handlers = {
+		function(lua_ls)
+			require("lspconfig")[lua_ls].setup({})
+		end,
+		function(pyright)
+			require("lspconfig")[pyright].setup({})
+		end,
+	},
 })
--- lua/plugin_config/mason.lua
+++ lua/plugin_config/mason.lua
@@ -1,13 +1,11 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"lua_ls",
-		-- "rust_analyzer",
-        "pyright",
-	}
+        ensure_installed = {
+                "lua_ls",
+                "pyright",
+        },
 })
 
-require("lspconfig").lua_ls.setup{}
+require("lspconfig").lua_ls.setup({})
 -- require("lspconfig").rust_analyzer.setup{}
-require("lspconfig").pyright.setup{}
-require("lspconfig").ruff.setup{}
+require("lspconfig").pyright.setup({})
