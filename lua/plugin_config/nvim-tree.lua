c26b4d4f2a06befff7f3a93a8ce980a7a81446c8
Alexandroskw <filipbilly.120@gmail.com>
2024-08-24 22:07:07 -0600

    Adding again NvimTree and changing the keymaps

--- lua/plugin_config/nvim-tree.lua
+++ lua/plugin_config/nvim-tree.lua
@@ -3,4 +3,5 @@ vim.g.loaded_newtrwPlugin = 1
 
 require("nvim-tree").setup()
 
-vim.keymap.set("n", "<C-p>", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>nn", ":NvimTreeFindFile<CR>")
