7c7111032c1ea3a4d0d2756dc4a1bd2273026f5d
Alexandroskw <filipbilly.120@gmail.com>
2024-08-24 22:05:31 -0600

    Ensuring install C and removing JS and TS

--- lua/plugin_config/treesitter.lua
+++ lua/plugin_config/treesitter.lua
@@ -1,31 +1,30 @@
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-  return
+        return
 end
 
 -- configure treesitter
 treesitter.setup({
-  -- enable syntax highlighting
-  highlight = {
-    enable = true,
-  },
-  -- enable indentation
-  indent = { enable = true },
-  -- ensure these language parsers are installed
-  ensure_installed = {
-	  "rust",
-	    "json",
-	    "javascript",
-	    "typescript",
-	    "yaml",
-	    "html",
-	    "css",
-	    "markdown",
-	    "markdown_inline",
-	    "bash",
-	    "lua",
-	    "vim"
-	  },
-  -- auto install above language parsers
-  auto_install = true,
+        -- enable syntax highlighting
+        highlight = {
+                enable = true,
+        },
+        -- enable indentation
+        indent = { enable = true },
+        -- ensure these language parsers are installed
+        ensure_installed = {
+                "rust",
+                "json",
+                "yaml",
+                "html",
+                "css",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "c"
+        },
+        -- auto install above language parsers
+        auto_install = true,
 })
